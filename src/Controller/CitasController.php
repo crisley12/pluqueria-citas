d8646d0610588d86c0bac43b6e804de93a95760c
josejmv <josejmvasquez@gmail.com>
2021-11-01 15:10:47 -0400

    validate max appointments for personal

--- src/Controller/CitasController.php
+++ src/Controller/CitasController.php
@@ -33,20 +33,30 @@ class CitasController extends AbstractController
     {
         $servicio = $this->getDoctrine()->getRepository(Servicios::class)->find(explode(' ',$_POST['servicio'])[0]);
         $personal = $this->getDoctrine()->getRepository(PersonalData::class)->find(explode(' ',$_POST['servicio'])[1]);
+        $citas = $this->getDoctrine()->getRepository(Citas::class)->findBy([
+            "PersonalData" => $personal,
+            "servicio" => $servicio,
+            "Fecha" => new \Datetime($_POST["fecha"])
+        ]);
         $cliente = $this->getDoctrine()->getRepository(Usuario::class)->find($_SESSION['user']->getId())->getClienteData();
 
-        $citas = new Citas();
-        $citas->setClienteData($cliente);
-        $citas->setServicio($servicio);
-        $citas->setPersonalData($personal); 
-        $citas->setFecha(new \Datetime($_POST["fecha"]));
-        $citas->setHora(new \Datetime($_POST["hora"]));
+        if(count($citas) < 3 ){
+            $citas = new Citas();
+            $citas->setClienteData($cliente);
+            $citas->setServicio($servicio);
+            $citas->setPersonalData($personal); 
+            $citas->setFecha(new \Datetime($_POST["fecha"]));
+            $citas->setHora(new \Datetime($_POST["hora"]));
 
-        $manager = $this->getDoctrine()->getManager();
-        $manager->persist($citas);
-        $manager->flush();
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($citas);
+            $manager->flush();
 
-        return $this->render('citas/successCita.html.twig');
+            return $this->render('citas/successCita.html.twig');
+        } else {
+            return $this->render('auth/index.html.twig');
+        }
+
     }
 
 }
